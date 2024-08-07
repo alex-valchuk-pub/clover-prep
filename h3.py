abbc30e18f55ccd1fa57a4ea77a48da37fc9f6fd
alex-valchuk-pub <alexander.valchuk@gmail.com>
2024-08-08 01:48:08 +0300

    feat: add QRCODE read

--- h3.py
+++ h3.py
@@ -2,11 +2,19 @@
 
 import math
 import rospy
+import cv2
 from clover import srv
 from std_srvs.srv import Trigger
 from sensor_msgs.msg import Range
 
+from pyzbar import pyzbar
+from pyzbar.pyzbar import ZBarSymbol
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge
+from clover import long_callback
+
 rospy.init_node('flight')
+bridge = CvBridge()
 
 get_telemetry = rospy.ServiceProxy('get_telemetry', srv.GetTelemetry)
 navigate = rospy.ServiceProxy('navigate', srv.Navigate)
@@ -22,6 +30,21 @@ def range_callback(msg):
     h = msg.range
 
 rospy.Subscriber('rangefinder/range', Range, range_callback)
+
+@long_callback
+def image_callback(msg):
+    img = bridge.imgmsg_to_cv2(msg, 'bgr8')
+    barcodes = pyzbar.decode(img, [ZBarSymbol.QRCODE])
+    for barcode in barcodes:
+        b_data = barcode.data.decode('utf-8')
+        b_type = barcode.type
+        (x, y, w, h) = barcode.rect
+        xc = x + w/2
+        yc = y + h/2
+        print('Found {} with data {} with center at x={}, y={}'.format(b_type, b_data, xc, yc))
+
+image_sub = rospy.Subscriber('main_camera/image_raw_throttled', Image, image_callback, queue_size=1)
+
 
 print('Take off and hover 1 m above the ground')
 navigate(x=0, y=0, z=1, frame_id='body', auto_arm=True)
